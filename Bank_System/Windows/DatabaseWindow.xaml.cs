3639c6d5f4df32c40633ce28a99541e42f2566fe
Atmosferrum <73058391+Atmosferrum@users.noreply.github.com>
2021-01-11 13:56:17 +0300

    First push
    Just first push

---
+++ Bank_System/Windows/DatabaseWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Database;
+
+namespace Bank_System.Windows
+{
+    /// <summary>
+    /// Interaction logic for DatabaseWindow.xaml
+    /// </summary>
+    public partial class DatabaseWindow : Window
+    {
+        SqlConnection connection;
+        SqlDataAdapter dataAdapter;
+        DataTable dataTable;
+
+        public DatabaseWindow()
+        {
+            InitializeComponent();
+            ConnectToDB();
+        }
+
+        public void ConnectToDB()
+        {
+            connection = new SqlConnection(Starter.ConnectionCreator().ConnectionString);
+            dataTable = new DataTable();
+            dataAdapter = new SqlDataAdapter();
+
+            dataAdapter.SelectCommand = new SqlCommand(Starter.SelectFromDB(), connection);
+
+            dataAdapter.Fill(dataTable);
+
+            DG_Database_View.DataContext = dataTable.DefaultView;
+        }
+    }
+}
